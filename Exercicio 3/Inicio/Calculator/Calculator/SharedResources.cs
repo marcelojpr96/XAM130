c36247368fe70c33158b715b7a2b2d9800fdb35f
DesktopDellMarcelo <marcelojpr96@gmail.com>
2018-03-19 14:10:47 +0000

    no message

---
+++ Exercicio 3/Inicio/Calculator/Calculator/SharedResources.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Calculator
+{
+   public static  class SharedResources
+    {
+        public static Color OpButtonBkColor
+        {
+            get { return Color.FromRgb(0xff, 0x5a, 0); }
+        }
+    }
+}
